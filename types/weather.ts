5f2614127f7d28c0e1e0c30054502d3abe05c35b
shozebhasan <shozebhasan@gmail.com>
2025-06-16 13:26:32 +0500

    Your update message

--- types/weather.ts
+++ types/weather.ts
@@ -1,17 +1,16 @@
-export interface WeatherData{
-    name: string,
-    main : {
-        temp:number,
-        humidity : number,
-        feels_like : number,
-    },
-    weather: Array<{
-        main : string,
-        icon : string,
-        description : string;
-    }>,
-    wind :{
-        speed : number,
-        
-    }
+export interface WeatherData {
+  name: string,
+  main: {
+    temp: number,
+    humidity: number,
+    feels_like: number,
+  },
+  weather: Array<{
+    main: string,
+    description: string,
+    icon: string
+  }>,
+  wind: {
+    speed: number,
+  }
 }
